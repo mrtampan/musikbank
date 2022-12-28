4a6d0fdecf8447f4b344b67a63e3a8ada2a0c776
Achmad Rivaldi <33930946+mrtampan@users.noreply.github.com>
2022-12-28 20:58:21 +0700

    Update generate.php

--- generate.php
+++ generate.php
@@ -1,9 +1,15 @@
 <?php
 
-$allFile = array("music" => glob("*.mp3"));
+$dataCategories = [];
+
+while($dirs = glob('*', GLOB_ONLYDIR)) {
+  $data[$dirs] = array("music" => glob("*.mp3"));
+}
+
+
 $dataMusic = fopen("data.json", "w") or die("Unable to open file!");
 
-fwrite($dataMusic, json_encode($allFile));
+fwrite($dataMusic, json_encode($dataCategories));
 
 fclose($dataMusic);
 ?>
